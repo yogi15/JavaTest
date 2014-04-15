96b6e49700b8dd0d3a9caf5c69cfb4ce61aa1a2a
Yogesh <KYogesh@DSK319-M3.quinnox.corp>
2014-04-15 13:09:51 +0530

    Second user edited

--- src/com/quinnox/test/JavaGitTest.java
+++ src/com/quinnox/test/JavaGitTest.java
@@ -13,5 +13,7 @@ public class JavaGitTest {
 		System.out.println("Some other developer edited this");
 		
 		System.out.println("Edited on 15th April");
+		
+		System.out.println("Edited on 15th April by second user");
 	}
 }
